ae9a1d2585ef998f4656acdc35cf263ee88e6dfa
Michael Sproul <michael@sigmaprime.io>
2023-10-20 12:29:18 +1100

    Delete fragile sizeof detection

--- build.rs
+++ build.rs
@@ -2,11 +2,6 @@ extern crate bindgen;
 extern crate cc;
 
 use std::env;
-use std::process::Command;
-
-use rand::Rng;
-use std::fs::{remove_file, File};
-use std::io::Write;
 use std::path::PathBuf;
 
 fn add_def(v: &mut Vec<(String, String)>, key: &str, val: &str) {
@@ -15,6 +10,14 @@ fn add_def(v: &mut Vec<(String, String)>, key: &str, val: &str) {
 
 fn main() {
     let mut defines = Vec::new();
+
+    let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
+
+    // Fail compilation on any non 64-bit platform.
+    if target_pointer_width != "64" {
+        panic!("Unsupported target pointer width: {}", target_pointer_width);
+    }
+
     for i in &[
         "size_t",
         "unsigned int",
@@ -22,7 +25,7 @@ fn main() {
         "unsigned long long",
     ] {
         let def_name = format!("SIZEOF_{}", i.to_uppercase().replace(" ", "_"));
-        defines.push((def_name, check_native_size(i)));
+        defines.push((def_name, "8".to_string()));
     }
     add_def(&mut defines, "SECONDARY_DJW", "1");
     add_def(&mut defines, "SECONDARY_FGK", "1");
@@ -73,46 +76,4 @@ fn main() {
             .write_to_file(out_path.join("bindings.rs"))
             .expect("Couldn't write bindings!");
     }
-}
-
-fn check_native_size(name: &str) -> String {
-    let builder = cc::Build::new();
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let compiler = builder.get_compiler();
-    let mut compile = Command::new(compiler.path().as_os_str());
-    let test_code = format!("#include <stdint.h>\n#include <stdio.h>\nint main() {{printf(\"%lu\", sizeof({})); return 0;}}\n", name);
-    // didn't use tempfile since tempfile was having issues on Windows
-    let mut rng = rand::thread_rng();
-    let test_binary_fn = format!("{}/test-{}", out_dir, rng.gen::<i32>());
-
-    #[cfg(windows)]
-    let test_binary_fn = format!("{}.exe", test_binary_fn);
-
-    let test_source_fn = format!("{}/test-{:x}.c", out_dir, rng.gen::<i32>());
-    let mut test_source = File::create(&test_source_fn).expect("Error creating test compile files");
-
-    compile.args(compiler.args()).current_dir(out_dir);
-    if compiler.is_like_msvc() {
-        compile.args(&[&test_source_fn, &format!("/Fe{}", test_binary_fn)]);
-    } else {
-        compile.args(&[&test_source_fn, "-o", &test_binary_fn]);
-    }
-    test_source
-        .write_all(test_code.as_bytes())
-        .expect("Error writing test compile files");
-    drop(test_source); // close the source file, otherwise there will be problems on Windows
-    for &(ref a, ref b) in compiler.env().iter() {
-        compile.env(a, b);
-    }
-    compile.output().expect("Error compiling test source");
-    remove_file(test_source_fn).ok();
-
-    compile = Command::new(&test_binary_fn);
-    let output = compile
-        .output()
-        .expect("Error executing test binary")
-        .stdout;
-    let output = String::from_utf8(output).expect("Error converting Unicode sequence");
-    remove_file(test_binary_fn).ok();
-    return output;
 }

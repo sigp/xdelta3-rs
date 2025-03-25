4db64086bb02e9febb584ba93b9d16bb2ae3825a
Michael Sproul <micsproul@gmail.com>
2025-03-25 16:53:16 +1100

    Add error codes and API for dealing with output buffer lengths (#5)
    * Propagate error codes in encode/decode
    
    * Allow output buffer length to be set manually
    
    * Handle ENOSPC nicely

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,8 @@
 //!
 //! You can find out how to use this crate in this documentation and you can also consult the tests in the `test/` folder
 //! to see it in action (how to generate and patch two files!)
-
-extern crate libc;
-
 use libc::c_uint;
+use std::ffi::CStr;
 
 #[cfg(feature = "stream")]
 pub mod stream;
@@ -25,6 +23,37 @@ mod binding {
     #![allow(non_snake_case)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+pub enum Error {
+    XDelta3 {
+        error_code: i32,
+    },
+    InsufficientOutputLength,
+    OutOfBounds {
+        expected_length: u32,
+        actual_length: u32,
+    },
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::XDelta3 { error_code } => unsafe {
+                let c_str = CStr::from_ptr(libc::strerror(*error_code));
+                write!(f, "XDelta3 {} ({})", c_str.to_string_lossy(), error_code)
+            },
+            Self::InsufficientOutputLength => {
+                write!(f, "InsufficientOutputLength")
+            }
+            Self::OutOfBounds {
+                expected_length,
+                actual_length,
+            } => {
+                write!(f, "OutOfBounds: {} > {}", actual_length, expected_length)
+            }
+        }
+    }
 }
 
 /// Function to generate the difference data
@@ -54,29 +83,47 @@ mod binding {
 /// You might notice the generated patch data is larger than both orginal data and the updated data.
 /// But don't worry, if your data is large enough and kind of similar between each other (usually the case
 /// for software updates or ROM patches), the patch data should be only a fraction of your updated file.
-pub fn encode(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
-    unsafe {
-        let input_len = input.len() as c_uint;
-        let src_len = src.len() as c_uint;
-        let estimated_out_len = (input_len + src_len) * 2;
-        let mut avail_output = 0 as c_uint;
-        let mut output = Vec::with_capacity(estimated_out_len as usize);
-        let result = binding::xd3_encode_memory(
+pub fn encode(input: &[u8], src: &[u8]) -> Result<Vec<u8>, Error> {
+    encode_with_output_len(input, src, (input.len() + src.len()) as u32 * 2)
+}
+
+pub fn encode_with_output_len(
+    input: &[u8],
+    src: &[u8],
+    output_buffer_len: u32,
+) -> Result<Vec<u8>, Error> {
+    let input_len = input.len() as c_uint;
+    let src_len = src.len() as c_uint;
+    let mut avail_output = 0 as c_uint;
+    let mut output = Vec::with_capacity(output_buffer_len as usize);
+    let error_code = unsafe {
+        binding::xd3_encode_memory(
             input.as_ptr(),
             input_len,
             src.as_ptr(),
             src_len,
             output.as_mut_ptr(),
             &mut avail_output,
-            estimated_out_len,
+            output_buffer_len,
             0,
-        );
-        if result == 0 {
-            output.set_len(avail_output as usize);
-            Some(output)
-        } else {
-            None
+        )
+    };
+    if error_code == 0 {
+        // Extra sanity check to prevent UB.
+        if avail_output > output_buffer_len {
+            return Err(Error::OutOfBounds {
+                expected_length: output_buffer_len,
+                actual_length: avail_output,
+            });
         }
+        unsafe {
+            output.set_len(avail_output as usize);
+        }
+        Ok(output)
+    } else if error_code == libc::ENOSPC {
+        Err(Error::InsufficientOutputLength)
+    } else {
+        Err(Error::XDelta3 { error_code })
     }
 }
 
@@ -103,28 +150,46 @@ pub fn encode(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
 ///     assert_eq!(result.unwrap().as_slice(), &[1, 2, 3, 4, 5, 6, 7]);
 /// }
 /// ```
-pub fn decode(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
-    unsafe {
-        let input_len = input.len() as c_uint;
-        let src_len = src.len() as c_uint;
-        let estimated_out_len = (input_len + src_len) * 2;
-        let mut avail_output = 0 as c_uint;
-        let mut output = Vec::with_capacity(estimated_out_len as usize);
-        let result = binding::xd3_decode_memory(
+pub fn decode(input: &[u8], src: &[u8]) -> Result<Vec<u8>, Error> {
+    decode_with_output_len(input, src, (input.len() + src.len()) as u32 * 2)
+}
+
+pub fn decode_with_output_len(
+    input: &[u8],
+    src: &[u8],
+    output_buffer_len: u32,
+) -> Result<Vec<u8>, Error> {
+    let input_len = input.len() as c_uint;
+    let src_len = src.len() as c_uint;
+    let mut avail_output = 0 as c_uint;
+    let mut output = Vec::with_capacity(output_buffer_len as usize);
+    let error_code = unsafe {
+        binding::xd3_decode_memory(
             input.as_ptr(),
             input_len,
             src.as_ptr(),
             src_len,
             output.as_mut_ptr(),
             &mut avail_output,
-            estimated_out_len,
+            output_buffer_len,
             0,
-        );
-        if result == 0 {
-            output.set_len(avail_output as usize);
-            Some(output)
-        } else {
-            None
+        )
+    };
+    if error_code == 0 {
+        // Extra sanity check to prevent UB.
+        if avail_output > output_buffer_len {
+            return Err(Error::OutOfBounds {
+                expected_length: output_buffer_len,
+                actual_length: avail_output,
+            });
         }
+        unsafe {
+            output.set_len(avail_output as usize);
+        }
+        Ok(output)
+    } else if error_code == libc::ENOSPC {
+        Err(Error::InsufficientOutputLength)
+    } else {
+        Err(Error::XDelta3 { error_code })
     }
 }
